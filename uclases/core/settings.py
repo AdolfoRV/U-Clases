a72766a8d083c39fa0db4dc65a9d43abd25e046b
AdolfoRV <adolfo.rojas.v@ug.uchile.cl>
2025-09-21 21:18:38 -0300

    feat: add accounts app to installed apps and URL routing

--- uclases/core/settings.py
+++ uclases/core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     
 	'home',
 	'user_auth',
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- uclases/core/urls.py
+++ uclases/core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('', include('user_auth.urls')),
 	path('', include('home.urls')),
+	path('', include('accounts.urls')),
 ]
